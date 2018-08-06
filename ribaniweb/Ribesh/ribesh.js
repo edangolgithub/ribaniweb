09869f44a8c03a20ebc0661ee4cecea0d41c3d0f
edangol <edangol@EVAN1>
2018-08-06 19:09:11 +0545

    tokyo ghoul

---
+++ ribaniweb/Ribesh/ribesh.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+
+    $("h1").css({ "color": "red","font-family":"algerian" , "font-size":"42px" });
+
+    $("#h").css({ "color": "white", "font-family": "microsoft sans serif" , "font-size":"22px" });
+
+    
+
+
+
+    $("h1").dblclick(function () {
+
+        alert("this website wants connect with you")
+
+    });
+
+
+
+
+});
