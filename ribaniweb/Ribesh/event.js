4ce842c3f39d86620cdc870cec245d3a5b6c78c1
ribesh amatya <princeribesh14@yahoo.com>
2018-08-10 20:18:51 +0545

    tgs2

---
+++ ribaniweb/Ribesh/event.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+
+    $("#j").click(function () {
+
+        alert(7);    });
+
+    $(".c").dblclick(function () {
+
+
+        $(this).text("i was kidding hahaha u fool");
+
+    });
+
+    $(".d").mouseenter(function () {
+
+
+        $(this).css({ "color": "red" });
+
+    });
+
+    $(".d").mouseleave(function () {
+
+
+        $(this).css({ "color": "black" });
+
+    });
+
+
+});
--- ribaniweb/Ribesh/reebesh.js
+++ ribaniweb/Ribesh/reebesh.js
@@ -1,8 +1,15 @@
 ﻿$(document).ready(function () {
 
-    $("h1").css({ "color": "lawngreen", "font-family": "algerian", "font-size": "42px" , "margin" });
+    $("#h").css({ "color": "lawngreen", "font-family": "algerian", "font-size": "42px", "margin": "10px" });
+    $("#r").css({ "color": "red", "font-family": "algerian", "font-size": "36px", "margin": "10px" });
+    $("#s").css({ "color": "white", "font-family": "algerian", "font-size": "18px", "margin": "10px" });
+
+    $(".t").click(function () {
 
 
+        alert("you opened Tokyo Ghoul s2");
+
+    });
 
 
 
