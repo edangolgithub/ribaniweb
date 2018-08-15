13841487470b96164a513849ef55ab2942ac3f96
evan dangol <dangolevan@gmail.com>
2018-08-15 18:01:05 +0545

    torpkt

---
+++ ribaniweb/Evan/evan.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+
+    //var w = $("#pp").parent();
+    //console.log(w);
+    //w.css({"background-color":"red"});
+
+    $("#pp").children().css({ "color": "red", "border": "2px solid red" });
+    
+    // $("div").children().css({ "color": "blue", "border": "2px solid red" });
+    $("#daju").siblings().css({
+        "color": "green",
+    });
+
+
+
+
+});
--- ribaniweb/Ribesh/ribesh.js
+++ ribaniweb/Ribesh/ribesh.js
@@ -28,18 +28,43 @@
     var text = $("#h").text();
     $("#h").css({ "cursor": "pointer" });
     $("#h").mousedown(function () {
-
-
         $(this).text("tokyo ghoul tokyo ghoul tokyo ghoul tokyo ghoul tokyo ghoultokyo ghoultokyo ghoultokyo ghoultokyo ghoul");
-
-    });
+   });
 
     $("#h").mouseup(function () {
         $(this).text(text);
-
     });
 
-    $("#b").click(function () {
-        $("#hhh").toggle(10000);
+    //$("#b").click(function () {
+    //    $("#hhh").animate({ left: '250px' });
+    //});
+    //$("button").click(function () {
+    //    $("div").animate({ left: '250px' });
+    //});
+    //$("#b").click(d);
+
+    //    function d () {
+    //    $("#hhh").animate({ left: '250px' });
+    //};
+
+    $("#b").click(ribesh);
+
+    function ribesh() {
+
+        $("#pp").addClass("blue");
+
+        $("#dd").text("start").fadeOut(1000).fadeIn(1000).hide(1000).show(1000,ddd);
+
+    };
+
+    function ddd() {
+        $("#dd").text("done");
+    }
+
+    $("#t").click(function () {
+        $.get("ajax.html", function (data, status) {
+            alert("Data: " + data + "\nStatus: " + status);
+            $("#r").text(data);
+        });
     });
 });
